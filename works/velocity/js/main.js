1fcca9c70a9b98e4fe6facba19f8b3757aa10061
Bohdan Zozulya <zozulya.bohdan@gmail.com>
2022-07-21 13:15:50 +0300

    Fixes commit

--- works/velocity/js/main.js
+++ works/velocity/js/main.js
@@ -1,42 +1,46 @@
-// TABS
-const tabButtons = document.querySelectorAll(".tabs__nav button");
-const tabsItems = document.querySelectorAll(".tabs .tabs__item");
+document.addEventListener('DOMContentLoaded', function () {
 
-// Function to hide tabs and remove active class from buttons
-function hideTabs() {
-	tabsItems.forEach(item => item.classList.add("hide"));
-	tabButtons.forEach(btn => btn.classList.remove("active"));
-}
+	// TABS
+	const tabButtons = document.querySelectorAll(".tabs__nav button");
+	const tabsItems = document.querySelectorAll(".tabs .tabs__item");
 
-// Shows number of Tab item and makes corresponding button active
-function showTab(idx) {
-	tabsItems[idx].classList.remove("hide");
-	tabButtons[idx].classList.add("active");
-}
+	// Function to hide tabs and remove active class from buttons
+	function hideTabs() {
+		tabsItems.forEach(item => item.classList.add("hide"));
+		tabButtons.forEach(btn => btn.classList.remove("active"));
+	}
 
-hideTabs();
-showTab(0);
+	// Shows number of Tab item and makes corresponding button active
+	function showTab(idx) {
+		tabsItems[idx].classList.remove("hide");
+		tabButtons[idx].classList.add("active");
+	}
 
-tabButtons.forEach((btn, i) => {
-	btn.addEventListener("click", () => {
-		hideTabs();
-		showTab(i);
-	});
-});
+	hideTabs();
+	showTab(0);
 
-// ANCHORS
-const anchors = document.querySelectorAll(".header__nav a");
-
-anchors.forEach(anc => {
-	anc.addEventListener("click", (e) => {
-		e.preventDefault();
-
-		const id = anc.getAttribute("href");
-		const elem = document.querySelector(id);
-
-		window.scroll({
-			top: elem.offsetTop - 80,
-			behavior: 'smooth',
+	tabButtons.forEach((btn, i) => {
+		btn.addEventListener("click", () => {
+			hideTabs();
+			showTab(i);
 		});
 	});
+
+	// ANCHORS
+	const anchors = document.querySelectorAll(".header__nav a");
+
+	anchors.forEach(anc => {
+		anc.addEventListener("click", (e) => {
+			e.preventDefault();
+
+			const id = anc.getAttribute("href");
+			const elem = document.querySelector(id);
+
+			window.scroll({
+				top: elem.offsetTop - 80,
+				behavior: 'smooth',
+			});
+		});
+	});
+	
 });
